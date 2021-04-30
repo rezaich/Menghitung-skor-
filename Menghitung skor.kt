2917f1c44fdda3241d59da29a766cab080dc6119
rezaich <79249523+rezaich@users.noreply.github.com>
2021-04-30 09:26:12 +0700

    file kt menghitung score

---
+++ Menghitung skor.kt
@@ -0,0 +1,13 @@
+fun main() {
+var skor = -1
+
+if (skor <= 100 && skor >= 81) { println("A")
+} else if (skor <= 80 && skor >= 71) {
+println("B")
+} else if (skor <= 70 && skor >= 61) {
+println("C")
+} else if (skor >= 0 && skor <= 60) {
+println("D")
+} else {
+print("Skor tidak diketahui")
+} }
